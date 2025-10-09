a0c0ad3ed031f6ec19e83720538e92e746efdb14
Trent <mr.peschke@gmail.com>
2025-10-09 13:20:20 -0600

    making it so that the firebackground doesnt rerender on each preload chapter fetch

--- app/src/components/fireBackground/FireBackground.tsx
+++ app/src/components/fireBackground/FireBackground.tsx
@@ -1,34 +1,37 @@
+import { memo } from 'react';
 import './FireBackground.css'
 
-export default function FireBackground() {
-    const colors = ['#b45f06', '#ff9a5a', '#ffd166', '#ffbc42', '#990000'];
+export default memo(
+    function FireBackground() {
+        const colors = ['#b45f06', '#ff9a5a', '#ffd166', '#ffbc42', '#990000'];
 
-    function createFlameStylings() {
-        const size = 10 + Math.random() * 20;
-        const leftPos = Math.random() * 100;
-        const delay = Math.random() * 15;
-        const duration = 5 + Math.random() * 5;
+        function createFlameStylings() {
+            const size = 10 + Math.random() * 20;
+            const leftPos = Math.random() * 100;
+            const delay = Math.random() * 15;
+            const duration = 5 + Math.random() * 5;
 
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-        return {
-            width: `${size}px`,
-            height: `${size * 1.2}px`,
-            left: `${leftPos}%`,
-            backgroundColor: randomColor,
-            animationDelay: `${delay}s`,
-            animationDuration: `${duration}s`
+            return {
+                width: `${size}px`,
+                height: `${size * 1.2}px`,
+                left: `${leftPos}%`,
+                backgroundColor: randomColor,
+                animationDelay: `${delay}s`,
+                animationDuration: `${duration}s`
+            }
         }
-    }
 
-    return (
-        <div className="fire-background-shell">
-            <div className='texturing'></div>
-            <div className="fire-container" id="headerFire">
-                {[...Array(200).keys()].map((_, index) => {
-                    return <div key={index} className='flame' style={createFlameStylings()}></div>
-                })}
+        return (
+            <div className="fire-background-shell">
+                <div className='texturing'></div>
+                <div className="fire-container" id="headerFire">
+                    {[...Array(200).keys()].map((_, index) => {
+                        return <div key={index} className='flame' style={createFlameStylings()}></div>
+                    })}
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
+)
