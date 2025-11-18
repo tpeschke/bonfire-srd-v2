5491d914c476c9c05ce0508a5b49723ea8e55926
peschke <mr.peschke@gmail.com>
2025-11-17 20:08:39 -0700

    edit icon doesnt display on search page

--- app/src/components/header/icons/EditIcon.tsx
+++ app/src/components/header/icons/EditIcon.tsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux'
 import { isOwner } from '../../../redux/slices/userSlice'
 import '../Header.css'
 import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
 
 interface Props {
     pathname: string
@@ -10,9 +11,15 @@ interface Props {
 export default function EditIcon({ pathname }: Props) {
     const userIsOwner = useSelector(isOwner)
 
+    const [pageIsEditable, setPageIsEditable] = useState(false)
+
+    useEffect(() => {
+        setPageIsEditable(pathname !== '/' && !pathname.includes('edit') && pathname.substring(0, 7) !== '/search')
+    }, [pathname])
+
     return (
         <>
-            {userIsOwner && pathname !== '/' && !pathname.includes('edit') &&
+            {userIsOwner && pageIsEditable &&
                 <Link className='header-spacer' to={`${pathname}/edit`}>
                     <i className="fa-solid fa-pen-to-square"></i>
                 </Link>
